e174ac48d2917e9628c37acf27e7b54781bcacf0
wnbell <wnbell@gmail.com>
2009-10-15 22:45:51 +0000

    reworked DIA SpMV kernel

--- cusp/detail/device/spmv/dia.h
+++ cusp/detail/device/spmv/dia.h
@@ -19,6 +19,7 @@
 
 #include <cusp/dia_matrix.h>
 
+#include <cusp/detail/device/common.h>
 #include <cusp/detail/device/utils.h>
 #include <cusp/detail/device/texture.h>
 
@@ -62,32 +63,67 @@ spmv_dia_kernel(const IndexType num_rows,
                       ValueType * y)
 {
     __shared__ int offsets[BLOCK_SIZE];
+    
+    const IndexType thread_id = blockDim.x * blockIdx.x + threadIdx.x;
+    const IndexType grid_size = gridDim.x * blockDim.x;
 
+    // load diagonal offsets into shared memory
     if(threadIdx.x < num_diagonals)
         offsets[threadIdx.x] = diagonal_offsets[threadIdx.x];
 
     __syncthreads();
 
-    const int row = large_grid_thread_id();
 
-    if(row >= num_rows){ return; }
-
-    ValueType sum = y[row];
-    values += row;
-
-    for(IndexType n = 0; n < num_diagonals; n++)
+    for(IndexType row = thread_id; row < num_rows; row += grid_size)
     {
-        const int col = row + offsets[n];
+        ValueType sum = y[row];
 
-        if(col >= 0 && col < num_cols){
-            const ValueType A_ij = *values;
-            sum += A_ij * fetch_x<UseCache>(col, x);
+        IndexType offset = row;
+
+        for(IndexType n = 0; n < num_diagonals; n++)
+        {
+            const int col = row + offsets[n];
+    
+            if(col >= 0 && col < num_cols)
+            {
+                const ValueType A_ij = values[offset];
+                sum += A_ij * fetch_x<UseCache>(col, x);
+            }
+    
+            offset += stride;
         }
 
-        values += stride;
+        y[row] = sum;
+    }
+}
+
+    
+template <bool UseCache, typename IndexType, typename ValueType>
+void __spmv_dia(const cusp::dia_matrix<IndexType,ValueType,cusp::device>& dia, 
+                const ValueType * x, 
+                      ValueType * y)
+{
+    const unsigned int BLOCK_SIZE = 256;
+    const unsigned int MAX_BLOCKS = MAX_THREADS / BLOCK_SIZE;
+    const unsigned int NUM_BLOCKS = std::min(MAX_BLOCKS, DIVIDE_INTO(dia.num_rows, BLOCK_SIZE));
+    
+    if (UseCache)
+        bind_x(x);
+  
+    // the dia_kernel only handles BLOCK_SIZE diagonals at a time
+    for(unsigned int base = 0; base < dia.num_diagonals; base += BLOCK_SIZE)
+    {
+        IndexType num_diagonals = std::min(dia.num_diagonals - base, BLOCK_SIZE);
+
+        spmv_dia_kernel<IndexType, ValueType, BLOCK_SIZE, UseCache> <<<NUM_BLOCKS, BLOCK_SIZE>>>
+            (dia.num_rows, dia.num_cols, num_diagonals, dia.stride,
+             thrust::raw_pointer_cast(&dia.diagonal_offsets[0]) + base,
+             thrust::raw_pointer_cast(&dia.values[0]) + base * dia.stride,
+             x, y);
     }
 
-    y[row] = sum;
+    if (UseCache)
+        unbind_x(x);
 }
 
 template <typename IndexType, typename ValueType>
@@ -95,23 +131,7 @@ void spmv_dia(const cusp::dia_matrix<IndexType,ValueType,cusp::device>& dia,
               const ValueType * x, 
                     ValueType * y)
 {
-    const unsigned int BLOCK_SIZE = 256;
-    const dim3 grid = make_large_grid(dia.num_rows, BLOCK_SIZE);
-    
-    const IndexType * diagonal_offsets = thrust::raw_pointer_cast(&dia.diagonal_offsets[0]);
-    const ValueType * values           = thrust::raw_pointer_cast(&dia.values[0]);
-  
-    // the dia_kernel only handles BLOCK_SIZE diagonals at a time
-    for(unsigned int base = 0; base < dia.num_diagonals; base += BLOCK_SIZE)
-    {
-        IndexType num_diagonals = std::min(dia.num_diagonals - base, BLOCK_SIZE);
-
-        spmv_dia_kernel<IndexType, ValueType, BLOCK_SIZE, false> <<<grid, BLOCK_SIZE>>>
-            (dia.num_rows, dia.num_cols, num_diagonals, dia.stride,
-             diagonal_offsets + base,
-             values + base * dia.stride,
-             x, y);
-    }
+    __spmv_dia<false>(dia, x, y);
 }
 
 template <typename IndexType, typename ValueType>
@@ -119,27 +139,7 @@ void spmv_dia_tex(const cusp::dia_matrix<IndexType,ValueType,cusp::device>& dia,
                   const ValueType * x, 
                         ValueType * y)
 {
-    const unsigned int BLOCK_SIZE = 256;
-    const dim3 grid = make_large_grid(dia.num_rows, BLOCK_SIZE);
-
-    const IndexType * diagonal_offsets = thrust::raw_pointer_cast(&dia.diagonal_offsets[0]);
-    const ValueType * values           = thrust::raw_pointer_cast(&dia.values[0]);
-
-    bind_x(x);
-  
-    // the dia_kernel only handles BLOCK_SIZE diagonals at a time
-    for(unsigned int base = 0; base < dia.num_diagonals; base += BLOCK_SIZE)
-    {
-        IndexType num_diagonals = std::min(dia.num_diagonals - base, BLOCK_SIZE);
-
-        spmv_dia_kernel<IndexType, ValueType, BLOCK_SIZE, true> <<<grid, BLOCK_SIZE>>>
-            (dia.num_rows, dia.num_cols, num_diagonals, dia.stride,
-             diagonal_offsets + base,
-             values + base * dia.stride,
-             x, y);
-    }
-
-    unbind_x(x);
+    __spmv_dia<true>(dia, x, y);
 }
 
 
