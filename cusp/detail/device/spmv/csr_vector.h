21c2161ebd5db385a6cf1dbf041236f8b4fbc990
wnbell <wnbell@gmail.com>
2009-10-16 04:12:37 +0000

    CSR (vector) SpMV kernel now supports variable vector length

--- cusp/detail/device/spmv/csr_vector.h
+++ cusp/detail/device/spmv/csr_vector.h
@@ -51,8 +51,11 @@ namespace device
 // spmv_csr_vector_tex_device
 //   Same as spmv_csr_vector_tex_device, except that the texture cache is 
 //   used for accessing the x vector.
+//  
+//  Note: THREADS_PER_VECTOR must be one of [2,4,8,16,32]
 
-template <typename IndexType, typename ValueType, unsigned int BLOCK_SIZE, bool UseCache>
+
+template <typename IndexType, typename ValueType, unsigned int VECTORS_PER_BLOCK, unsigned int THREADS_PER_VECTOR, bool UseCache>
 __global__ void
 spmv_csr_vector_kernel(const IndexType num_rows,
                        const IndexType * Ap, 
@@ -61,51 +64,52 @@ spmv_csr_vector_kernel(const IndexType num_rows,
                        const ValueType * x, 
                              ValueType * y)
 {
-    __shared__ ValueType sdata[BLOCK_SIZE + 16];                          // padded to avoid reduction ifs
-    __shared__ IndexType ptrs[BLOCK_SIZE/WARP_SIZE][2];
+    __shared__ ValueType sdata[(VECTORS_PER_BLOCK + 1) * THREADS_PER_VECTOR];      // padded to avoid reduction conditionals
+    __shared__ IndexType ptrs[VECTORS_PER_BLOCK][2];
     
-    const IndexType thread_id   = BLOCK_SIZE * blockIdx.x + threadIdx.x;  // global thread index
-    const IndexType thread_lane = threadIdx.x & (WARP_SIZE-1);            // thread index within the warp
-    const IndexType warp_id     = thread_id   / WARP_SIZE;                // global warp index
-    const IndexType warp_lane   = threadIdx.x / WARP_SIZE;                // warp index within the CTA
-    const IndexType num_warps   = (BLOCK_SIZE / WARP_SIZE) * gridDim.x;   // total number of active warps
+    const IndexType THREADS_PER_BLOCK = VECTORS_PER_BLOCK * THREADS_PER_VECTOR;
 
-    for(IndexType row = warp_id; row < num_rows; row += num_warps)
+    const IndexType thread_id   = THREADS_PER_BLOCK * blockIdx.x + threadIdx.x;    // global thread index
+    const IndexType thread_lane = threadIdx.x & (THREADS_PER_VECTOR - 1);          // thread index within the vector
+    const IndexType vector_id   = thread_id   /  THREADS_PER_VECTOR;               // global vector index
+    const IndexType vector_lane = threadIdx.x /  THREADS_PER_VECTOR;               // vector index within the block
+    const IndexType num_vectors = VECTORS_PER_BLOCK * gridDim.x;                   // total number of active vectors
+
+    for(IndexType row = vector_id; row < num_rows; row += num_vectors)
     {
         // use two threads to fetch Ap[row] and Ap[row+1]
         // this is considerably faster than the straightforward version
         if(thread_lane < 2)
-            ptrs[warp_lane][thread_lane] = Ap[row + thread_lane];
-        const IndexType row_start = ptrs[warp_lane][0];                   //same as: row_start = Ap[row];
-        const IndexType row_end   = ptrs[warp_lane][1];                   //same as: row_end   = Ap[row+1];
+            ptrs[vector_lane][thread_lane] = Ap[row + thread_lane];
 
-        // compute local sum
+        const IndexType row_start = ptrs[vector_lane][0];                   //same as: row_start = Ap[row];
+        const IndexType row_end   = ptrs[vector_lane][1];                   //same as: row_end   = Ap[row+1];
+
+        // initialize local sum
         ValueType sum = 0;
-        for(IndexType jj = row_start + thread_lane; jj < row_end; jj += WARP_SIZE)
+        
+        // accumulate local sums
+        for(IndexType jj = row_start + thread_lane; jj < row_end; jj += THREADS_PER_VECTOR)
             sum += Ax[jj] * fetch_x<UseCache>(Aj[jj], x);
 
-        // reduce local sums to row sum (ASSUME: warpsize 32)
+        // store local sum in shared memory
         sdata[threadIdx.x] = sum;
-        sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x + 16];
-        sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  8];
-        sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  4];
-        sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  2];
-        sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  1];
+        
+        // reduce local sums to row sum
+        if (THREADS_PER_VECTOR > 16) sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x + 16];
+        if (THREADS_PER_VECTOR >  8) sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  8];
+        if (THREADS_PER_VECTOR >  4) sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  4];
+        if (THREADS_PER_VECTOR >  2) sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  2];
+        if (THREADS_PER_VECTOR >  1) sdata[threadIdx.x] = sum = sum + sdata[threadIdx.x +  1];
        
 //// Alternative method (slightly slower)
-//        // compute local sum
-//        sdata[threadIdx.x] = 0;
-//        for(IndexType jj = row_start + thread_lane; jj < row_end; jj += WARP_SIZE)
-//            sdata[threadIdx.x] += Ax[jj] * fetch_x<UseCache>(Aj[jj], x);
-//
-//        // reduce local sums to row sum (ASSUME: warpsize 32)
-//        sdata[threadIdx.x] += sdata[threadIdx.x + 16];
-//        sdata[threadIdx.x] += sdata[threadIdx.x +  8];
-//        sdata[threadIdx.x] += sdata[threadIdx.x +  4];
-//        sdata[threadIdx.x] += sdata[threadIdx.x +  2];
-//        sdata[threadIdx.x] += sdata[threadIdx.x +  1];
+//      if (THREADS_PER_VECTOR > 16)  sdata[threadIdx.x] += sdata[threadIdx.x + 16];
+//      if (THREADS_PER_VECTOR >  8)  sdata[threadIdx.x] += sdata[threadIdx.x +  8];
+//      if (THREADS_PER_VECTOR >  4)  sdata[threadIdx.x] += sdata[threadIdx.x +  4];
+//      if (THREADS_PER_VECTOR >  2)  sdata[threadIdx.x] += sdata[threadIdx.x +  2];
+//      if (THREADS_PER_VECTOR >  1)  sdata[threadIdx.x] += sdata[threadIdx.x +  1];
 
-        // first thread writes warp result
+        // first thread writes the result
         if (thread_lane == 0)
             y[row] += sdata[threadIdx.x];
     }
@@ -116,15 +120,18 @@ void __spmv_csr_vector(const csr_matrix<IndexType,ValueType,cusp::device>& csr,
                        const ValueType * x, 
                              ValueType * y)
 {
-    const unsigned int BLOCK_SIZE = 128;
-    const unsigned int WARPS_PER_BLOCK = BLOCK_SIZE / WARP_SIZE;
-    const unsigned int MAX_BLOCKS = thrust::experimental::arch::max_active_blocks(spmv_csr_vector_kernel<IndexType, ValueType, BLOCK_SIZE, UseCache>, BLOCK_SIZE, (size_t) 0);
-    const unsigned int NUM_BLOCKS = std::min(MAX_BLOCKS, DIVIDE_INTO(csr.num_rows, WARPS_PER_BLOCK));
+    const unsigned int THREADS_PER_VECTOR = 32;
+    const unsigned int VECTORS_PER_BLOCK  =  4;
+    const unsigned int THREADS_PER_BLOCK  = VECTORS_PER_BLOCK * THREADS_PER_VECTOR;
+
+    //const unsigned int MAX_BLOCKS = MAX_THREADS / THREADS_PER_BLOCK;
+    const unsigned int MAX_BLOCKS = thrust::experimental::arch::max_active_blocks(spmv_csr_vector_kernel<IndexType, ValueType, VECTORS_PER_BLOCK, THREADS_PER_VECTOR, UseCache>, THREADS_PER_BLOCK, (size_t) 0);
+    const unsigned int NUM_BLOCKS = std::min(MAX_BLOCKS, DIVIDE_INTO(csr.num_rows, VECTORS_PER_BLOCK));
     
     if (UseCache)
         bind_x(x);
 
-    spmv_csr_vector_kernel<IndexType, ValueType, BLOCK_SIZE, UseCache> <<<NUM_BLOCKS, BLOCK_SIZE>>> 
+    spmv_csr_vector_kernel<IndexType, ValueType, VECTORS_PER_BLOCK, THREADS_PER_VECTOR, UseCache> <<<NUM_BLOCKS, THREADS_PER_BLOCK>>> 
         (csr.num_rows,
          thrust::raw_pointer_cast(&csr.row_offsets[0]),
          thrust::raw_pointer_cast(&csr.column_indices[0]),
