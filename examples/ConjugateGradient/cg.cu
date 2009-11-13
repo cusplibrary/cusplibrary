c67e480a9b8280f5ef5617dd40bb6beccc5a296f
wnbell <wnbell@gmail.com>
2009-11-13 21:40:40 +0000

    fixed ConjugateGradient example

--- examples/ConjugateGradient/cg.cu
+++ examples/ConjugateGradient/cg.cu
@@ -5,28 +5,34 @@
 // where to perform the computation
 typedef cusp::device_memory MemorySpace;
 
+// which floating point type to use
+typedef float ValueType;
+
 int main(void)
 {
     // create an empty sparse matrix structure (HYB format)
-    cusp::hyb_matrix<int, float, MemorySpace> A;
+    cusp::hyb_matrix<int, ValueType, MemorySpace> A;
 
     // load a matrix stored in MatrixMarket format
     cusp::read_matrix_market_file(A, "5pt_10x10.mtx");
 
     // allocate storage for solution (x) and right hand side (b)
-    cusp::array1d<float, MemorySpace> x(A.num_rows, 0);
-    cusp::array1d<float, MemorySpace> b(A.num_rows, 1);
+    cusp::array1d<ValueType, MemorySpace> x(A.num_rows, 0);
+    cusp::array1d<ValueType, MemorySpace> b(A.num_rows, 1);
 
     // set stopping criteria:
     //        tolerance = 1e-6
     //  iteration_limit = 100
     cusp::default_stopping_criteria stopping_criteria(1e-6, 100);
 
-    // set verbose flag
-    bool verbose = true;
+    // set preconditioner (identity)
+    cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
+
+    // set verbosity level
+    int verbose = 1;
 
     // obtain a linear operator from matrix A and call CG
-    cusp::krylov::cg(A, x, b, stopping_criteria, verbose);
+    cusp::krylov::cg(A, x, b, stopping_criteria, M, verbose);
 
     return 0;
 }
