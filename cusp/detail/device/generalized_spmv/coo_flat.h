b85bd9783a5dc1ac0d2ed26cca877f11430e064b
wnbell <wnbell@gmail.com>
2010-10-17 10:49:19 -0400

    generalized SpMV for COO now works correctly for one CUDA block

--- cusp/detail/device/generalized_spmv/coo_flat.h
+++ cusp/detail/device/generalized_spmv/coo_flat.h
@@ -33,6 +33,54 @@ namespace device
 namespace cuda
 {
 
+//template <int BLOCK_SIZE,
+//          typename SizeType,
+//          typename IndexIterator1,
+//          typename IndexIterator2,
+//          typename ValueIterator1,
+//          typename ValueIterator2,
+//          typename ValueIterator4,
+//          typename BinaryFunction1,
+//          typename BinaryFunction2>
+//__launch_bounds__(BLOCK_SIZE,1)
+//__global__
+//void spmv_coo_kernel(SizeType        num_entries,
+//                     IndexIterator1  row_indices,
+//                     IndexIterator2  column_indices,
+//                     ValueIterator1  values,
+//                     ValueIterator2  x, 
+//                     ValueIterator4  z,
+//                     BinaryFunction1 combine,
+//                     BinaryFunction2 reduce)
+//{
+//  typedef typename thrust::iterator_value<IndexIterator1>::type IndexType1;
+//  typedef typename thrust::iterator_value<IndexIterator2>::type IndexType2;
+//  typedef typename thrust::iterator_value<ValueIterator1>::type ValueType1;
+//  typedef typename thrust::iterator_value<ValueIterator2>::type ValueType2;
+//  typedef typename thrust::iterator_value<ValueIterator4>::type ValueType4;
+//
+//  if (threadIdx.x == 0)
+//  {
+//    for (SizeType i = 0; i < num_entries; i++)
+//    {
+//      IndexType1 i   = thrust::detail::device::dereference(row_indices);
+//      IndexType2 j   = thrust::detail::device::dereference(column_indices);
+//      ValueType1 Aij = thrust::detail::device::dereference(values);
+//
+//      ValueIterator2 tmp_x = x + j;  ValueType2 xj = thrust::detail::device::dereference(tmp_x);
+//      ValueIterator4 tmp_z = z + i;  ValueType4 zi = thrust::detail::device::dereference(tmp_z);
+//
+//      thrust::detail::device::dereference(tmp_z) = reduce(zi, combine(Aij, xj));
+//
+//      ++row_indices;
+//      ++column_indices;
+//      ++values;
+//    }
+//  }
+//}
+
+const int K = 5;
+
 template <int BLOCK_SIZE,
           typename SizeType,
           typename IndexIterator1,
@@ -59,9 +107,141 @@ void spmv_coo_kernel(SizeType        num_entries,
   typedef typename thrust::iterator_value<ValueIterator2>::type ValueType2;
   typedef typename thrust::iterator_value<ValueIterator4>::type ValueType4;
 
+  __shared__ IndexType1 rows[K][BLOCK_SIZE + 1];
+  __shared__ ValueType4 vals[K][BLOCK_SIZE + 1];
+
+  __shared__ IndexType1 row_carry;
+  __shared__ ValueType4 val_carry;
+
+  __syncthreads(); // is this really necessary?
+
+  SizeType interval_begin = 0;
+  SizeType interval_end   = num_entries;
+
+  SizeType unit_size = K * BLOCK_SIZE;
+ 
+  SizeType base = interval_begin;
+
+  row_indices    += base;
+  column_indices += base;
+  values         += base;
+
+  // process full units
+  while(base + unit_size < interval_end)
+  {
+    // read data
+    for(int k = 0; k < K; k++)
+    {
+      int offset = BLOCK_SIZE * k + threadIdx.x;
+
+      IndexIterator1 _i   = row_indices    + offset;
+      IndexIterator2 _j   = column_indices + offset;
+      ValueIterator1 _Aij = values         + offset;
+
+      ValueIterator2 _xj = x + thrust::detail::device::dereference(_j);
+
+      rows[offset % K][offset / K] = thrust::detail::device::dereference(_i);
+      vals[offset % K][offset / K] = combine(thrust::detail::device::dereference(_Aij), thrust::detail::device::dereference(_xj));
+    }
+
+    // carry in
+    if (threadIdx.x == 0 && base != interval_begin)
+    {
+      if (row_carry == rows[0][0])
+      {
+        // row continues into this unit
+        vals[0][0] = reduce(val_carry, vals[0][0]);
+      }
+      else
+      {
+        // row terminates in previous unit
+        ValueIterator4 _zj = z + row_carry;
+        thrust::detail::device::dereference(_zj) = reduce(thrust::detail::device::dereference(_zj), val_carry);
+      }
+    }
+      
+    __syncthreads();
+
+    // process local values
+    for(int k = 1; k < K; k++)
+    {
+      if (rows[k][threadIdx.x] == rows[k - 1][threadIdx.x])
+        vals[k][threadIdx.x] = reduce(vals[k - 1][threadIdx.x], vals[k][threadIdx.x]);
+    }
+
+    __syncthreads();
+
+    // process across block
+    
+    // TODO replace with parallel implementation
+    if (threadIdx.x == 0)
+    {
+      for(int i = 1; i < BLOCK_SIZE; i++)
+      {
+        if (rows[K - 1][i] == rows[K - 1][i - 1])
+          vals[K - 1][i] = reduce(vals[K - 1][i - 1], vals[K - 1][i]);
+      }
+    
+    }
+
+    __syncthreads();
+
+    if (threadIdx.x == 0)
+    {
+      // update carry and sentinel value
+      row_carry = rows[0][BLOCK_SIZE] = rows[K - 1][BLOCK_SIZE - 1];
+      val_carry = vals[0][BLOCK_SIZE] = vals[K - 1][BLOCK_SIZE - 1];
+    }
+    else
+    {
+      // update local values
+      for(int k = 0; k < K - 1; k++)
+      {
+        IndexType1 row = rows[K - 1][threadIdx.x - 1];
+        ValueType4 val = vals[K - 1][threadIdx.x - 1];
+
+        if(rows[k][threadIdx.x] == row)
+        {
+          vals[k][threadIdx.x] = reduce(val, vals[k][threadIdx.x]);
+        }
+      }
+    }
+
+    __syncthreads();
+
+    // write data
+    for(int k = 0; k < K; k++)
+    {
+      int offset = BLOCK_SIZE * k + threadIdx.x;
+
+      if (rows[offset % K][offset / K] != rows[(offset + 1) % K][(offset + 1) / K])
+      {
+        // row terminates
+        ValueIterator4 _zj = z + rows[offset % K][offset / K];
+        thrust::detail::device::dereference(_zj) = reduce(thrust::detail::device::dereference(_zj), vals[offset % K][offset / K]);
+      }
+    }
+
+    // advance iterators
+    base           += unit_size;
+    row_indices    += unit_size;
+    column_indices += unit_size;
+    values         += unit_size;
+
+    __syncthreads();
+  }
+
+
+  // process the tail
   if (threadIdx.x == 0)
   {
-    for (SizeType i = 0; i < num_entries; i++)
+    if (base != interval_begin)
+    {
+      ValueIterator4 _zj = z + row_carry;
+      thrust::detail::device::dereference(_zj) = reduce(thrust::detail::device::dereference(_zj), val_carry);
+    }
+
+    while(base < num_entries)
     {
       IndexType1 i   = thrust::detail::device::dereference(row_indices);
       IndexType2 j   = thrust::detail::device::dereference(column_indices);
@@ -71,7 +251,8 @@ void spmv_coo_kernel(SizeType        num_entries,
       ValueIterator4 tmp_z = z + i;  ValueType4 zi = thrust::detail::device::dereference(tmp_z);
 
       thrust::detail::device::dereference(tmp_z) = reduce(zi, combine(Aij, xj));
-
+    
+      ++base;
       ++row_indices;
       ++column_indices;
       ++values;
@@ -101,7 +282,7 @@ void spmv_coo(SizeType        num_rows,
               BinaryFunction1 combine,
               BinaryFunction2 reduce)
 {
-  const SizeType block_size = 256;
+  const SizeType block_size = 128;
   //const SizeType max_blocks = cusp::detail::device::arch::max_active_blocks(spmv_coo_scalar_kernel<block_size, SizeType, IndexIterator1, IndexIterator2, ValueIterator1, ValueIterator2, ValueIterator4, BinaryFunction1, BinaryFunction2>, block_size, (size_t) 0);
   const SizeType num_blocks = 1; //std::min(max_blocks, DIVIDE_INTO(num_entries, block_size));
 
--- testing/generalized_spmv.cu
+++ testing/generalized_spmv.cu
@@ -1,17 +1,85 @@
 #include <unittest/unittest.h>
 
-#include <cusp/detail/device/generalized_spmv/csr_scalar.h>
 #include <cusp/detail/device/generalized_spmv/coo_flat.h>
+#include <cusp/detail/device/generalized_spmv/csr_scalar.h>
 
-#include <cusp/csr_matrix.h>
 #include <cusp/array1d.h>
+#include <cusp/coo_matrix.h>
+#include <cusp/csr_matrix.h>
+#include <cusp/multiply.h>
 #include <cusp/gallery/poisson.h>
+#include <cusp/gallery/random.h>
 
-void TestCsrGeneralizedSpMV(void)
+template <typename Matrix,
+          typename Array1,
+          typename Array2,
+          typename Array3,
+          typename UnaryFunction,
+          typename BinaryFunction1,
+          typename BinaryFunction2>
+void generalized_spmv(const Matrix& A,
+                      const Array1& x,
+                      const Array2& y,
+                            Array3& z,
+                      UnaryFunction   initialize,
+                      BinaryFunction1 combine,
+                      BinaryFunction2 reduce,
+                      cusp::csr_format)
 {
-    typedef cusp::csr_matrix<int,float,cusp::device_memory> TestMatrix;
-    typedef typename TestMatrix::memory_space MemorySpace;
+    cusp::detail::device::cuda::spmv_csr_scalar
+        (A.num_rows,
+         A.row_offsets.begin(), A.column_indices.begin(), A.values.begin(),
+         x.begin(), y.begin(), z.begin(),
+         initialize, combine, reduce);
+}
 
+template <typename Matrix,
+          typename Array1,
+          typename Array2,
+          typename Array3,
+          typename UnaryFunction,
+          typename BinaryFunction1,
+          typename BinaryFunction2>
+void generalized_spmv(const Matrix& A,
+                      const Array1& x,
+                      const Array2& y,
+                            Array3& z,
+                      UnaryFunction   initialize,
+                      BinaryFunction1 combine,
+                      BinaryFunction2 reduce,
+                      cusp::coo_format)
+{
+    cusp::detail::device::cuda::spmv_coo
+        (A.num_rows, A.num_entries,
+         A.row_indices.begin(), A.column_indices.begin(), A.values.begin(),
+         x.begin(), y.begin(), z.begin(),
+         initialize, combine, reduce);
+}
+
+template <typename Matrix,
+          typename Array1,
+          typename Array2,
+          typename Array3,
+          typename UnaryFunction,
+          typename BinaryFunction1,
+          typename BinaryFunction2>
+void generalized_spmv(const Matrix& A,
+                      const Array1& x,
+                      const Array2& y,
+                            Array3& z,
+                      UnaryFunction   initialize,
+                      BinaryFunction1 combine,
+                      BinaryFunction2 reduce)
+{
+  generalized_spmv(A, x, y, z, initialize, combine, reduce, typename Matrix::format());
+}
+
+template <typename TestMatrix>
+void _TestGeneralizedSpMV(void)
+{
+  typedef typename TestMatrix::memory_space MemorySpace;
+
+  {
     // initialize example matrix
     cusp::array2d<float, cusp::host_memory> A(5,4);
     A(0,0) = 13; A(0,1) = 80; A(0,2) =  0; A(0,3) =  0; 
@@ -26,7 +94,8 @@ void TestCsrGeneralizedSpMV(void)
     // allocate vectors
     cusp::array1d<float, MemorySpace> x(4);
     cusp::array1d<float, MemorySpace> y(5);
-   
+    cusp::array1d<float, MemorySpace> z(5,-1);
+
     // initialize input and output vectors
     x[0] = 1.0f; y[0] = 10.0f; 
     x[1] = 2.0f; y[1] = 20.0f;
@@ -34,58 +103,67 @@ void TestCsrGeneralizedSpMV(void)
     x[3] = 4.0f; y[3] = 40.0f;
                  y[4] = 50.0f;
 
-    cusp::detail::device::cuda::spmv_csr_scalar
-        (test_matrix.num_rows,
-         test_matrix.row_offsets.begin(), test_matrix.column_indices.begin(), test_matrix.values.begin(),
-         x.begin(), y.begin(), y.begin(),
-         thrust::identity<float>(), thrust::multiplies<float>(), thrust::plus<float>());
-                                               
-    ASSERT_EQUAL(y[0], 183.0f);
-    ASSERT_EQUAL(y[1],  74.0f);
-    ASSERT_EQUAL(y[2], 325.0f);
-    ASSERT_EQUAL(y[3], 510.0f);
-    ASSERT_EQUAL(y[4], 131.0f);
+    generalized_spmv(test_matrix, x, y, z, thrust::identity<float>(), thrust::multiplies<float>(), thrust::plus<float>());
+
+    ASSERT_EQUAL(z[0], 183.0f);
+    ASSERT_EQUAL(z[1],  74.0f);
+    ASSERT_EQUAL(z[2], 325.0f);
+    ASSERT_EQUAL(z[3], 510.0f);
+    ASSERT_EQUAL(z[4], 131.0f);
+  }
+  
+  
+  {
+    TestMatrix A;
+
+    cusp::gallery::poisson5pt(A, 117, 113);
+
+    // allocate vectors
+    cusp::array1d<float, MemorySpace> x(A.num_cols,1);
+    cusp::array1d<float, MemorySpace> y(A.num_rows,0);
+    cusp::array1d<float, MemorySpace> z(A.num_rows,10);
+
+    generalized_spmv(A, x, y, z, thrust::identity<float>(), thrust::multiplies<float>(), thrust::plus<float>());
+  
+    // compute reference
+    cusp::array1d<float, MemorySpace> reference(A.num_rows,0);
+    cusp::multiply(A, x, reference);
+
+    ASSERT_EQUAL(z, reference);
+  }
+  
+  {
+    TestMatrix A;
+
+    cusp::gallery::random(128, 128, 17, A);
+
+    // allocate vectors
+    cusp::array1d<float, MemorySpace> x(A.num_cols,1);
+    cusp::array1d<float, MemorySpace> y(A.num_rows,0);
+    cusp::array1d<float, MemorySpace> z(A.num_rows,10);
+
+    generalized_spmv(A, x, y, z, thrust::identity<float>(), thrust::multiplies<float>(), thrust::plus<float>());
+  
+    // compute reference
+    cusp::array1d<float, MemorySpace> reference(A.num_rows,0);
+    cusp::multiply(A, x, reference);
+
+    ASSERT_EQUAL(z, reference);
+  }
+}
+
+
+void TestCsrGeneralizedSpMV(void)
+{
+  typedef cusp::csr_matrix<int,float,cusp::device_memory> TestMatrix;
+  _TestGeneralizedSpMV<TestMatrix>();
 }
 DECLARE_UNITTEST(TestCsrGeneralizedSpMV);
 
 void TestCooGeneralizedSpMV(void)
 {
-    typedef cusp::coo_matrix<int,float,cusp::device_memory> TestMatrix;
-    typedef typename TestMatrix::memory_space MemorySpace;
-
-    // initialize example matrix
-    cusp::array2d<float, cusp::host_memory> A(5,4);
-    A(0,0) = 13; A(0,1) = 80; A(0,2) =  0; A(0,3) =  0; 
-    A(1,0) =  0; A(1,1) = 27; A(1,2) =  0; A(1,3) =  0;
-    A(2,0) = 55; A(2,1) =  0; A(2,2) = 24; A(2,3) = 42;
-    A(3,0) =  0; A(3,1) = 69; A(3,2) =  0; A(3,3) = 83;
-    A(4,0) =  0; A(4,1) =  0; A(4,2) = 27; A(4,3) =  0;
-
-    // convert to desired format
-    TestMatrix test_matrix = A;
-
-    // allocate vectors
-    cusp::array1d<float, MemorySpace> x(4);
-    cusp::array1d<float, MemorySpace> y(5);
-   
-    // initialize input and output vectors
-    x[0] = 1.0f; y[0] = 10.0f; 
-    x[1] = 2.0f; y[1] = 20.0f;
-    x[2] = 3.0f; y[2] = 30.0f;
-    x[3] = 4.0f; y[3] = 40.0f;
-                 y[4] = 50.0f;
-
-    cusp::detail::device::cuda::spmv_coo
-        (test_matrix.num_rows, test_matrix.num_entries,
-         test_matrix.row_indices.begin(), test_matrix.column_indices.begin(), test_matrix.values.begin(),
-         x.begin(), y.begin(), y.begin(),
-         thrust::identity<float>(), thrust::multiplies<float>(), thrust::plus<float>());
-                                               
-    ASSERT_EQUAL(y[0], 183.0f);
-    ASSERT_EQUAL(y[1],  74.0f);
-    ASSERT_EQUAL(y[2], 325.0f);
-    ASSERT_EQUAL(y[3], 510.0f);
-    ASSERT_EQUAL(y[4], 131.0f);
+  typedef cusp::coo_matrix<int,float,cusp::device_memory> TestMatrix;
+  _TestGeneralizedSpMV<TestMatrix>();
 }
 DECLARE_UNITTEST(TestCooGeneralizedSpMV);
 
