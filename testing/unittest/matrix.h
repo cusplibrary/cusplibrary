071178e331783825947f7952eca26da6bb2733f9
wnbell <wnbell@gmail.com>
2010-11-07 16:06:27 -0500

    refactored matrix test instantiation

--- testing/unittest/matrix.h
+++ testing/unittest/matrix.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// TODO create double versions that inspect the device capabilities
+
 // Macro to create host and device versions of a unit test
 #define DECLARE_HOST_DEVICE_UNITTEST(VTEST)                    \
 void VTEST##Host(void)   {  VTEST< cusp::host_memory   >(); }  \
@@ -7,32 +9,49 @@ void VTEST##Device(void) {  VTEST< cusp::device_memory >(); }  \
 DECLARE_UNITTEST(VTEST##Host);                                 \
 DECLARE_UNITTEST(VTEST##Device);
 
-// Macro to create host and device versions of a unit test
-#define DECLARE_HOST_SPARSE_MATRIX_UNITTEST(VTEST)                                                                                                                \
-void VTEST##CooMatrixHost(void)   { VTEST< cusp::coo_matrix<int,float,cusp::host_memory> >();  VTEST< cusp::coo_matrix<long long,float,cusp::host_memory> >(); }  \
-void VTEST##CsrMatrixHost(void)   { VTEST< cusp::csr_matrix<int,float,cusp::host_memory> >();  VTEST< cusp::csr_matrix<long long,float,cusp::host_memory> >(); }  \
-void VTEST##DiaMatrixHost(void)   { VTEST< cusp::dia_matrix<int,float,cusp::host_memory> >();  VTEST< cusp::dia_matrix<long long,float,cusp::host_memory> >(); }  \
-void VTEST##EllMatrixHost(void)   { VTEST< cusp::ell_matrix<int,float,cusp::host_memory> >();  VTEST< cusp::ell_matrix<long long,float,cusp::host_memory> >(); }  \
-void VTEST##HybMatrixHost(void)   { VTEST< cusp::hyb_matrix<int,float,cusp::host_memory> >();  VTEST< cusp::hyb_matrix<long long,float,cusp::host_memory> >(); }  \
-DECLARE_UNITTEST(VTEST##CooMatrixHost);                                                                                                                           \
-DECLARE_UNITTEST(VTEST##CsrMatrixHost);                                                                                                                           \
-DECLARE_UNITTEST(VTEST##DiaMatrixHost);                                                                                                                           \
-DECLARE_UNITTEST(VTEST##EllMatrixHost);                                                                                                                           \
-DECLARE_UNITTEST(VTEST##HybMatrixHost);                                                              
+////////////////
+// Containers //
+////////////////
 
-#define DECLARE_DEVICE_SPARSE_MATRIX_UNITTEST(VTEST)                                                                                                                 \
-void VTEST##CooMatrixDevice(void) { VTEST< cusp::coo_matrix<int,float,cusp::device_memory> >(); VTEST< cusp::coo_matrix<long long,float,cusp::device_memory> >(); }  \
-void VTEST##CsrMatrixDevice(void) { VTEST< cusp::csr_matrix<int,float,cusp::device_memory> >(); VTEST< cusp::csr_matrix<long long,float,cusp::device_memory> >(); }  \
-void VTEST##DiaMatrixDevice(void) { VTEST< cusp::dia_matrix<int,float,cusp::device_memory> >(); VTEST< cusp::dia_matrix<long long,float,cusp::device_memory> >(); }  \
-void VTEST##EllMatrixDevice(void) { VTEST< cusp::ell_matrix<int,float,cusp::device_memory> >(); VTEST< cusp::ell_matrix<long long,float,cusp::device_memory> >(); }  \
-void VTEST##HybMatrixDevice(void) { VTEST< cusp::hyb_matrix<int,float,cusp::device_memory> >(); VTEST< cusp::hyb_matrix<long long,float,cusp::device_memory> >(); }  \
-DECLARE_UNITTEST(VTEST##CooMatrixDevice);                                                                                                                            \
-DECLARE_UNITTEST(VTEST##CsrMatrixDevice);                                                                                                                            \
-DECLARE_UNITTEST(VTEST##DiaMatrixDevice);                                                                                                                            \
-DECLARE_UNITTEST(VTEST##EllMatrixDevice);                                                                                                                            \
-DECLARE_UNITTEST(VTEST##HybMatrixDevice);                                                            
+// Dense Matrix Containers
+#define DECLARE_DENSE_MATRIX_UNITTEST(VTEST)                                                                            \
+void VTEST##Array2dRowMajorHost(void)      { VTEST< cusp::array2d<float,cusp::host_memory  ,cusp::row_major   > >(); }  \
+void VTEST##Array2dRowMajorDevice(void)    { VTEST< cusp::array2d<float,cusp::device_memory,cusp::row_major   > >(); }  \
+void VTEST##Array2dColumnMajorHost(void)   { VTEST< cusp::array2d<float,cusp::host_memory  ,cusp::column_major> >(); }  \
+void VTEST##Array2dColumnMajorDevice(void) { VTEST< cusp::array2d<float,cusp::device_memory,cusp::column_major> >(); }  \
+DECLARE_UNITTEST(VTEST##Array2dRowMajorHost);                                                                           \
+DECLARE_UNITTEST(VTEST##Array2dRowMajorDevice);                                                                         \
+DECLARE_UNITTEST(VTEST##Array2dColumnMajorHost);                                                                        \
+DECLARE_UNITTEST(VTEST##Array2dColumnMajorDevice);                                                                                                         
+
+// Sparse Matrix Containers 
+#define DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Fmt,fmt)                                                                                                                           \
+void VTEST##Fmt##MatrixHost(void)   { VTEST< cusp::fmt##_matrix<int,float,cusp::host_memory> >();    VTEST< cusp::fmt##_matrix<long long,float,cusp::host_memory> >();   }  \
+void VTEST##Fmt##MatrixDevice(void) { VTEST< cusp::fmt##_matrix<int,float,cusp::device_memory> >();  VTEST< cusp::fmt##_matrix<long long,float,cusp::device_memory> >(); }  \
+DECLARE_UNITTEST(VTEST##Fmt##MatrixHost);                                                                                                                                       \
+DECLARE_UNITTEST(VTEST##Fmt##MatrixDevice);                                                                                                                                     
 
 #define DECLARE_SPARSE_MATRIX_UNITTEST(VTEST) \
-DECLARE_HOST_SPARSE_MATRIX_UNITTEST(VTEST)    \
-DECLARE_DEVICE_SPARSE_MATRIX_UNITTEST(VTEST)
+DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Coo,coo) \
+DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Csr,csr) \
+DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Dia,dia) \
+DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Ell,ell) \
+DECLARE_SPARSE_FORMAT_UNITTEST(VTEST,Hyb,hyb)
+
+// All Matrix Containers 
+#define DECLARE_MATRIX_UNITTEST(VTEST) \
+DECLARE_DENSE_MATRIX_UNITTEST(VTEST);  \
+DECLARE_SPARSE_MATRIX_UNITTEST(VTEST);
+
+///////////
+// Views //
+///////////
+
+// Sparse Matrix Views
+#define DECLARE_SPARSE_FORMAT_VIEW_UNITTEST(VTEST,Fmt,fmt)                                                                                                                                      \
+void VTEST##Fmt##MatrixViewHost(void)   { VTEST< cusp::fmt##_matrix<int,float,cusp::host_memory>::view   >(); VTEST< cusp::fmt##_matrix<long long,float,cusp::host_memory  >::view >(); }  \
+void VTEST##Fmt##MatrixViewDevice(void) { VTEST< cusp::fmt##_matrix<int,float,cusp::device_memory>::view >(); VTEST< cusp::fmt##_matrix<long long,float,cusp::device_memory>::view >(); }  \
+DECLARE_UNITTEST(VTEST##Fmt##MatrixViewHost);                                                                                                                                                  \
+DECLARE_UNITTEST(VTEST##Fmt##MatrixViewDevice);                                                                                                                                     
+
 
