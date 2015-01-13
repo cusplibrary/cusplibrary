6b743302cd1eb7926d1e762ab5573f0f5277a406
steve <sdalton1@gmail.com>
2015-01-13 13:54:15 -0800

    Minor fix to host DIA spmv

--- cusp/detail/host/spmv.h
+++ cusp/detail/host/spmv.h
@@ -141,22 +141,22 @@ void spmv_dia(const Matrix&  A,
 
     for(size_t i = 0; i < num_diagonals; i++)
     {
-        const IndexType& k = A.diagonal_offsets[i];
+        const IndexType k = A.diagonal_offsets[i];
 
-        const IndexType& i_start = std::max<IndexType>(0, -k);
-        const IndexType& j_start = std::max<IndexType>(0,  k);
+        const IndexType i_start = std::max<IndexType>(0, -k);
+        const IndexType j_start = std::max<IndexType>(0,  k);
 
         // number of elements to process in this diagonal
         const IndexType N = std::min(A.num_rows - i_start, A.num_cols - j_start);
 
         for(IndexType n = 0; n < N; n++)
         {
-            const ValueType& Aij = A.values(i_start + n, i);
+            const ValueType Aij = A.values(i_start + n, i);
 
-            const ValueType& xj = x[j_start + n];
-                  ValueType& yi = y[i_start + n];
-    
-            yi = reduce(yi, combine(Aij, xj));
+            const ValueType xj = x[j_start + n];
+                  ValueType yi = y[i_start + n];
+
+            y[i_start + n] = reduce(yi, combine(Aij, xj));
         }
     }
 }
