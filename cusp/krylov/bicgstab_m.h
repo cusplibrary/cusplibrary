fc793880640a5c7ea4378e65070eaaa6f091ca7e
Steve <sdalton1@gmail.com>
2015-06-29 21:27:59 -0700

    Updated multi-mass BiCGStab to use policies

--- cusp/krylov/bicgstab_m.h
+++ cusp/krylov/bicgstab_m.h
@@ -22,128 +22,24 @@
 
 #include <cusp/detail/config.h>
 
+#include <cusp/execution_policy.h>
+
 namespace cusp
 {
 namespace krylov
 {
+/*! \addtogroup iterative_solvers Iterative Solvers
+ *  \addtogroup krylov_methods Krylov Methods
+ *  \ingroup iterative_solvers
+ *  \{
+ */
 
 /* \cond */
-// TODO move to bicgstab_m.inl
-namespace trans_m
-{
-  template <typename InputIterator1, typename InputIterator2,
-            typename InputIterator3,
-	    typename OutputIterator1,
-	    typename ScalarType>
-  void compute_z_m(InputIterator1 z_0_s_b, InputIterator1 z_0_s_e,
-		InputIterator2 z_m1_s_b, InputIterator3 sig_b,
-		OutputIterator1 z_1_s_b,
-		ScalarType beta_m1, ScalarType beta_0, ScalarType alpha_0);
-
-  template <typename InputIterator1, typename InputIterator2,
-	    typename OutputIterator1,
-	    typename ScalarType>
-  void compute_b_m(InputIterator1 z_1_s_b, InputIterator1 z_1_s_e,
-		InputIterator2 z_0_s_b, OutputIterator1 beta_0_s_b,
-		ScalarType beta_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename Array4, typename ScalarType>
-  void compute_z_m(const Array1& z_0_s, const Array2& z_m1_s,
-		const Array3& sig, Array4& z_1_s,
-		ScalarType beta_m1, ScalarType beta_0, ScalarType alpha_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename ScalarType>
-  void compute_b_m(const Array1& z_1_s, const Array2& z_0_s,
-		Array3& beta_0_s, ScalarType beta_0);
-
-  template <typename InputIterator1, typename InputIterator2,
-            typename InputIterator3, typename OutputIterator,
-            typename ScalarType>
-  void compute_a_m(InputIterator1 z_0_s_b, InputIterator1 z_0_s_e,
-		InputIterator2 z_1_s_b, InputIterator3 beta_0_s_b,
-                OutputIterator alpha_0_s_b,
-		ScalarType beta_0, ScalarType alpha_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename Array4, typename ScalarType>
-  void compute_a_m(const Array1& z_0_s, const Array2& z_1_s,
-                const Array3& beta_0_s, Array4& alpha_0_s,
-		ScalarType beta_0, ScalarType alpha_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename Array4, typename Array5, typename Array6, typename Array7>
-  void compute_x_m(const Array1& beta_0_s, const Array2& chi_0_s,
-                const Array3& rho_0_s, const Array4& zeta_1_s,
-                const Array5& w_1, const Array6& s_0_s, Array7& x_0_s);
-  
-  template <typename Array1, typename Array2, typename Array3, typename Array4,
-	   typename Array5, typename Array6, typename Array7, typename Array8,
-	   typename Array9, typename Array10, typename Array11>
-  void compute_s_m(const Array1& beta_0_s, const Array2& chi_0_s,
-                const Array3& rho_0_s, const Array4& zeta_0_s,
-                const Array5& alpha_1_s, const Array6& rho_1_s,
-		const Array7& zeta_1_s,
-                const Array8& r_0, Array9& r_1,
-                const Array10& w_1, Array11& s_0_s);
-
-  template <typename InputIterator1, typename InputIterator2,
-	    typename OutputIterator, typename ScalarType>
-  void compute_w_1_m(InputIterator1 r_0_b, InputIterator1 r_0_e,
-		    InputIterator2 As_b, OutputIterator w_1_b,
-		    const ScalarType beta_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename ScalarType>
-  void compute_w_1_m(const Array1& r_0, const Array2& As, Array3& w_1,
-		  const ScalarType beta_0);
-
-  template <typename InputIterator1, typename InputIterator2,
-	    typename OutputIterator, typename ScalarType>
-  void compute_r_1_m(InputIterator1 w_1_b, InputIterator1 w_1_e,
-		    InputIterator2 Aw_b, OutputIterator r_1_b,
-		    ScalarType chi_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename ScalarType>
-  void compute_r_1_m(const Array1& w_1, const Array2& Aw, Array3& r_1,
-		  ScalarType chi_0);
-
-  template <typename InputIterator1, typename InputIterator2,
-	    typename OutputIterator, typename ScalarType>
-  void compute_s_0_m(InputIterator1 r_1_b, InputIterator1 r_1_e,
-		    InputIterator2 As_b, OutputIterator s_0_b,
-		    ScalarType alpha_1, ScalarType chi_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-            typename ScalarType>
-  void compute_s_0_m(const Array1& r_1, const Array2& As, Array3& s_0,
-		  ScalarType alpha_1, ScalarType chi_0);
-
-  template <typename InputIterator, typename OutputIterator,
-	    typename ScalarType>
-  void compute_chi_m(InputIterator sigma_b, InputIterator sigma_e,
-		     OutputIterator chi_0_s_b, ScalarType chi_0);
-
-  template <typename Array1, typename Array2, typename ScalarType>
-  void compute_chi_m(const Array1& sigma, Array2& chi_0_s, ScalarType chi_0);
-
-  template <typename InputIterator1, typename InputIterator2,
-	    typename OutputIterator, typename ScalarType>
-  void compute_rho_m(InputIterator1 rho_0_s_b, InputIterator1 rho_0_s_e,
-		     InputIterator2 sigma_b, OutputIterator rho_1_s_b,
-		     ScalarType chi_0);
-
-  template <typename Array1, typename Array2, typename Array3,
-	    typename ScalarType>
-  void compute_rho_m(const Array1& rho_0_s, const Array2& sigma,
-		  Array3& rho_1_s, ScalarType chi_0);
-
-  template <typename Array1, typename Array2>
-    void vectorize_copy(const Array1& source, Array2& dest);
-
-} // end namespace trans_m
+template <typename DerivedPolicy, class LinearOperator,
+          class VectorType1, class VectorType2, class VectorType3>
+void bicgstab_m(const thrust::detail::execution_policy_base<DerivedPolicy> &exec,
+                LinearOperator& A,
+                VectorType1& x, VectorType2& b, VectorType3& sigma);
 
 /**
  * \brief Multi-mass Biconjugate Gradient stabilized method
@@ -151,14 +47,17 @@ namespace trans_m
 template <class LinearOperator,
           class VectorType1, class VectorType2, class VectorType3>
 void bicgstab_m(LinearOperator& A,
-        VectorType1& x, VectorType2& b, VectorType3& sigma);
-/* \endcond */
+                VectorType1& x, VectorType2& b, VectorType3& sigma);
 
-/*! \addtogroup iterative_solvers Iterative Solvers
- *  \addtogroup krylov_methods Krylov Methods
- *  \ingroup iterative_solvers
- *  \{
- */
+template <typename DerivedPolicy,
+          class LinearOperator,
+          class VectorType1, class VectorType2, class VectorType3,
+          class Monitor>
+void bicgstab_m(const thrust::detail::execution_policy_base<DerivedPolicy> &exec,
+                LinearOperator& A,
+                VectorType1& x, VectorType2& b, VectorType3& sigma,
+                Monitor& monitor);
+/* \endcond */
 
 /**
  * \brief Multi-mass Biconjugate Gradient stabilized method
@@ -236,8 +135,8 @@ template <class LinearOperator,
           class VectorType1, class VectorType2, class VectorType3,
           class Monitor>
 void bicgstab_m(LinearOperator& A,
-        VectorType1& x, VectorType2& b, VectorType3& sigma,
-        Monitor& monitor);
+                VectorType1& x, VectorType2& b, VectorType3& sigma,
+                Monitor& monitor);
 /*! \}
  */
 
