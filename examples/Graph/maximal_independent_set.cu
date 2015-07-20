a5300c4a856466f54b1c0ff822974aac52d45428
Steve <sdalton1@gmail.com>
2015-07-20 09:33:07 -0700

    Uncommented testing cases and moved MIS example to graph folder

--- examples/Graph/maximal_independent_set.cu
+++ examples/Graph/maximal_independent_set.cu
@@ -1,21 +1,39 @@
-#include <cusp/csr_matrix.h>
-
-#include <cusp/gallery/poisson.h>
 #include <cusp/graph/maximal_independent_set.h>
+#include <cusp/gallery/poisson.h>
+#include <cusp/coo_matrix.h>
 
-int main(int argc, char*argv[])
+// This example computes a maximal independent set (MIS)
+// for a 10x10 grid.  The graph for the 10x10 grid is
+// described by the sparsity pattern of a sparse matrix
+// corresponding to a 10x10 Poisson problem.
+//
+// [1] http://en.wikipedia.org/wiki/Maximal_independent_set
+
+int main(void)
 {
-    // create an empty sparse matrix structure (CSR format)
-    cusp::csr_matrix<int,float,cusp::device_memory> G;
+    size_t N = 10;
 
-    // create a 2d Poisson problem on a 512x512 mesh
-    cusp::gallery::poisson5pt(G, 512, 512);
+    // initialize matrix representing 10x10 grid
+    cusp::coo_matrix<int, float, cusp::device_memory> G;
+    cusp::gallery::poisson5pt(G, N, N);
 
-    // create vector to contain MIS labels
-    cusp::array1d<bool,cusp::device_memory> stencil(G.num_rows);
+    // allocate storage for the MIS
+    cusp::array1d<int, cusp::device_memory> stencil(G.num_rows);
 
-    // execute MIS computation on device
+    // compute the MIS
     cusp::graph::maximal_independent_set(G, stencil);
+
+    // print MIS as a 2d grid
+    std::cout << "maximal independent set (marked with Xs)\n";
+    for (size_t i = 0; i < N; i++)
+    {
+        std::cout << "  ";
+        for (size_t j = 0; j < N; j++)
+        {
+            std::cout << ((stencil[N * i + j]) ? "X" : "0");
+        }
+        std::cout << "\n";
+    }
 
     return 0;
 }
--- testing/spectral_radius.cu
+++ testing/spectral_radius.cu
@@ -20,37 +20,37 @@ void TestEstimateSpectralRadius(void)
     typedef typename SparseMatrix::memory_space MemorySpace;
 
     // 2x2 diagonal matrix
-    /* { */
-    /*     cusp::csr_matrix<int,ValueType,cusp::host_memory> A(2,2,2); */
-    /*     A.row_offsets[0] = 0; */
-    /*     A.row_offsets[1] = 1; */
-    /*     A.row_offsets[2] = 2; */
-    /*     A.column_indices[0] = 0; */
-    /*     A.column_indices[1] = 1; */
-    /*     A.values[0] = -5; */
-    /*     A.values[1] =  2; */
-    /*  */
-    /*     float rho = 5.0; */
-    /*  */
-    /*     SparseMatrix B(A); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(B) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(B) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(B,10,true) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(B) - rho) / rho) < 0.1f, true); */
-    /* } */
-    /*  */
-    /* // 2x2 Poisson problem */
-    /* { */
-    /*     SparseMatrix A; */
-    /*     cusp::gallery::poisson5pt(A, 2, 2); */
-    /*  */
-    /*     float rho = 6.0; */
-    /*  */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(A) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A,10,true) - rho) / rho) < 0.1f, true); */
-    /*     ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(A) - rho) / rho) < 0.1f, true); */
-    /* } */
+    {
+        cusp::csr_matrix<int,ValueType,cusp::host_memory> A(2,2,2);
+        A.row_offsets[0] = 0;
+        A.row_offsets[1] = 1;
+        A.row_offsets[2] = 2;
+        A.column_indices[0] = 0;
+        A.column_indices[1] = 1;
+        A.values[0] = -5;
+        A.values[1] =  2;
+
+        float rho = 5.0;
+
+        SparseMatrix B(A);
+        ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(B) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(B) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(B,10,true) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(B) - rho) / rho) < 0.1f, true);
+    }
+
+    // 2x2 Poisson problem
+    {
+        SparseMatrix A;
+        cusp::gallery::poisson5pt(A, 2, 2);
+
+        float rho = 6.0;
+
+        ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(A) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A,10,true) - rho) / rho) < 0.1f, true);
+        ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(A) - rho) / rho) < 0.1f, true);
+    }
 
     // 4x4 Poisson problem
     {
@@ -59,11 +59,10 @@ void TestEstimateSpectralRadius(void)
 
         float rho = 7.2360679774997871;
 
-        /* ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(A) - rho) / rho) < 0.1f, true); */
-        std::cout << cusp::eigen::ritz_spectral_radius(A) << std::endl;
+        ASSERT_EQUAL((std::abs(cusp::eigen::estimate_spectral_radius(A) - rho) / rho) < 0.1f, true);
         ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A) - rho) / rho) < 0.1f, true);
         ASSERT_EQUAL((std::abs(cusp::eigen::ritz_spectral_radius(A,10,true) - rho) / rho) < 0.1f, true);
-        /* ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(A) - rho) / rho) < 0.11f, true); */
+        ASSERT_EQUAL((std::abs(cusp::eigen::disks_spectral_radius(A) - rho) / rho) < 0.11f, true);
     }
 
     // TODO test larger sizes and non-symmetric matrices
