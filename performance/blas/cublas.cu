9766c23ff962706613008d715778e023aded6835
Steve <sdalton1@gmail.com>
2016-05-01 17:02:24 -0700

    Minor updates to blas tests

--- performance/blas/cublas.cu
+++ performance/blas/cublas.cu
@@ -1,5 +1,8 @@
 #include <cusp/array1d.h>
+#include <cusp/array2d.h>
 #include <cusp/blas/blas.h>
+
+#include <cusp/system/cuda/detail/cublas/blas.h>
 
 #include <iostream>
 #include <stdio.h>
@@ -99,16 +102,47 @@ struct test_axpy
     size_t bytes(void) const { return 3 * n * sizeof(T); }
 };
 
+template <typename T, typename MemorySpace=cusp::device_memory, typename Orientation=cusp::column_major>
+struct test_gemm
+{
+    cusp::array2d<T,MemorySpace,Orientation> A, B;
+    const size_t n;
+    cublasHandle_t handle;
+
+    test_gemm(const size_t n)
+        : n(n), A(n,n), B(n,n)
+    {
+      if(cublasCreate(&handle) != CUBLAS_STATUS_SUCCESS)
+      {
+        throw cusp::runtime_exception("cublasCreate failed");
+      }
+    }
+
+    ~test_gemm()
+    {
+      cublasDestroy(handle);
+    }
+
+    void operator()(void)
+    {
+        cusp::blas::gemm(cusp::cuda::par.with(handle), A, A, B);
+    }
+
+    std::string name(void) const { return (sizeof(T) == 4) ? "sgemm" : "dgemm"; }
+
+    size_t bytes(void) const { return A.num_rows * B.num_rows * B.num_cols * sizeof(T); }
+};
 
 template <typename Test>
-void benchmark(const size_t n)
+void benchmark(const size_t n, const size_t iterations = 5)
 {
     Test test(n);
     test();
 
     timer t0;
-    test();
-    float ms = t0.milliseconds_elapsed();
+    for(size_t i = 0; i < iterations; i++)
+        test();
+    float ms = t0.milliseconds_elapsed() / iterations;
     float bw = (test.bytes() / (ms / 1e3)) / 1e9;
 
     printf("%-10s %6.1f GB/s [ %8.3f ms]\n", test.name().c_str(), bw, ms);
@@ -127,6 +161,17 @@ int main(int argc, char ** argv)
         benchmark< test_dot <double> >(n);
         benchmark< test_axpy<float>  >(n);
         benchmark< test_axpy<double> >(n);
+    }
+
+    printf("\n");
+
+    for (size_t n = 900; n < 1500; n += 100)
+    {
+        std::cout << "N = " << n << std::endl;
+        benchmark< test_gemm<float,  cusp::device_memory, cusp::row_major>    >(n);
+        benchmark< test_gemm<float,  cusp::device_memory, cusp::column_major> >(n);
+        benchmark< test_gemm<double, cusp::device_memory, cusp::row_major>    >(n);
+        benchmark< test_gemm<double, cusp::device_memory, cusp::column_major> >(n);
     }
 
     return 0;
