1c572ef589f4074f10371e4ed0113d176302eb67
Steve <sdalton1@gmail.com>
2016-05-07 19:59:45 -0700

    Added corner case for copy_if to grapple

--- performance/grapple/generate_policy.py
+++ performance/grapple/generate_policy.py
@@ -18,65 +18,74 @@ def process_file(project_base_dir, fobj, filename, base_list, project_name) :
             base_dir,base_file = split(filename);
             start_dir = os.path.split(base_dir)[1];
 
-            impl_file = join(base_dir, "detail", splitext(base_file)[0] + ".inl");
-            if os.path.isfile(impl_file):
-                with open(impl_file, "r") as fobj1:
+            if base_file == "copy.h" and project_name == "cusp" :
+                return;
 
-                    ext_policy_tag_str = 'template\s*?<\s*?typename DerivedPolicy.*?>.*?\{.*?\}';
-                    text = fobj1.read();
-                    output = re.findall(ext_policy_tag_str, text, re.DOTALL);
+            impl_file_list = [join(base_dir, "detail", splitext(base_file)[0] + ".inl")];
 
-                    if output :
-                        fobj.write("#include <{}>\n".format(inc_filename));
+            if base_file == "copy.h" and project_name == "thrust" :
+                impl_file_list.extend([join(base_dir, "detail", splitext(base_file)[0] + "_if.inl")]);
 
-                        for routine in output :
-                            if '::execution_policy<' in routine :
-                                continue;
+            for impl_file in impl_file_list:
+                print "processing {}".format(impl_file);
+                if os.path.isfile(impl_file):
+                    with open(impl_file, "r") as fobj1:
 
-                            name_search = re.search("using.*::(?P<name>\w+);", routine);
-                            if name_search :
-                                name = name_search.group("name")
-                            else :
-                                continue;
+                        ext_policy_tag_str = 'template\s*?<\s*?typename DerivedPolicy.*?>.*?\{.*?\}';
+                        text = fobj1.read();
+                        output = re.findall(ext_policy_tag_str, text, re.DOTALL);
 
-                            routine = re.sub("typename\s*?DerivedPolicy,\s*?", "", routine);
-                            routine = re.sub("__host__\s*__device__", "", routine);
-                            routine = re.sub("const thrust::detail::execution_policy_base<DerivedPolicy>", "my_policy", routine);
-                            routine = re.sub("thrust::detail::derived_cast\(thrust::detail::strip_const\(exec\)\)",
-                                             "exec.get()" if name not in base_list else "exec.base()", routine);
+                        if output :
+                            fobj.write("#include <{}>\n".format(inc_filename));
 
-                            ret = re.search(r"template\s*?<.*?>\s*(?P<return_type>[\w\d,:<> ]+)\s*" + name + r"\s*?\(", routine, re.DOTALL);
-                            if not ret:
-                                raise ValueError("\n"+routine)
-                            return_type = ret.group("return_type").strip();
+                            for routine in output :
+                                if '::execution_policy<' in routine :
+                                    continue;
 
-                            if "blas" in routine :
-                                name = "_".join(["blas", name]);
+                                name_search = re.search("using.*::(?P<name>\w+);", routine);
+                                if name_search :
+                                    name = name_search.group("name")
+                                else :
+                                    continue;
 
-                            lines = routine.split("\n")
+                                routine = re.sub("typename\s*?DerivedPolicy,\s*?", "", routine);
+                                routine = re.sub("__host__\s*__device__", "", routine);
+                                routine = re.sub("const thrust::detail::execution_policy_base<DerivedPolicy>", "my_policy", routine);
+                                routine = re.sub("thrust::detail::derived_cast\(thrust::detail::strip_const\(exec\)\)",
+                                                 "exec.get()" if name not in base_list else "exec.base()", routine);
 
-                            out_lines = []
-                            for line in lines:
-                                if line :
-                                    if "return" in line :
-                                        enum_id = "__{}_{}__".format(project_name.upper(), name.upper());
-                                        if enum_id not in enum_ids :
-                                            enum_ids.append(enum_id);
-                                        out_lines.append("\n  exec.start({});".format(enum_id));
-                                        if "void" in return_type:
-                                            out_lines.append("  " + line.split("return ",1)[1]);
+                                ret = re.search(r"template\s*?<.*?>\s*(?P<return_type>[\w\d,:<> ]+)\s*" + name + r"\s*?\(", routine, re.DOTALL);
+                                if not ret:
+                                    raise ValueError("\n"+routine)
+                                return_type = ret.group("return_type").strip();
+
+                                if "blas" in routine :
+                                    name = "_".join(["blas", name]);
+
+                                lines = routine.split("\n")
+
+                                out_lines = []
+                                for line in lines:
+                                    if line :
+                                        if "return" in line :
+                                            enum_id = "__{}_{}__".format(project_name.upper(), name.upper());
+                                            if enum_id not in enum_ids :
+                                                enum_ids.append(enum_id);
+                                            out_lines.append("\n  exec.start({});".format(enum_id));
+                                            if "void" in return_type:
+                                                out_lines.append("  " + line.split("return ",1)[1]);
+                                            else :
+                                                out_lines.append("  {} ret = {}".format(return_type, line.split("return ",1)[1]));
+                                            out_lines.append("  exec.stop();");
+
+                                            if "void" not in return_type:
+                                                out_lines.append("\n  return ret;");
                                         else :
-                                            out_lines.append("  {} ret = {}".format(return_type, line.split("return ",1)[1]));
-                                        out_lines.append("  exec.stop();");
-
-                                        if "void" not in return_type:
-                                            out_lines.append("\n  return ret;");
-                                    else :
-                                        out_lines.append(line)
+                                            out_lines.append(line)
 
 
-                            routine = "\n".join(out_lines);
-                            fobj.write("\n" + routine + "\n\n");
+                                routine = "\n".join(out_lines);
+                                fobj.write("\n" + routine + "\n\n");
 
     return enum_ids;
 
